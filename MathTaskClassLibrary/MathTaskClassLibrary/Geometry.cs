7d1cb8656d990494b8feca1e113e6c02217b926c
Хмиловский Владислав Михайлович ПИБ12 <Ufo.horek@gmail.com>
2023-06-09 16:45:02 +0700

    Описание изменений

---
+++ MathTaskClassLibrary/MathTaskClassLibrary/Geometry.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MathTaskClassLibrary
+{
+    public class Geometry
+    {
+        public int CalculateArea(int a,int b) //Тут просто если числа меньше 0, то бросаем исключение, в противном случае, все отлично!
+        {
+            if (a < 0 || b < 0) throw new ArgumentException();
+            return a * b;
+        }
+
+        public string GenerateAlphabetString(int N) //Тут промежуток выводимых чисел, сколько введешь, столько и выведется
+        {
+            if (N < 1 || N > 26)
+            {
+                throw new ArgumentException("Вводное значение должно быть между 1 и 26"); //Как раз тут бросаем исклчение, если условие не верно!
+            }
+
+            char startChar = 'A'; //С какой буквы начинаем
+            char endChar = (char)(startChar + N - 1); //Тут на какой заканчиваем
+
+            string alphabetString = string.Empty;
+            for (char c = startChar; c <= endChar; c++) //Тут обычный перебор и добавление по букве
+            {
+                alphabetString += c; 
+            }
+
+            return alphabetString;//тут возвращаем занчение
+        }
+
+        public double[] SolveQuadraticEquation(double a, double b, double c)
+        {
+            if (a < 0) throw new ArgumentException(); //Тут так же бросаем исключение если a < 0
+
+            double discriminant = b * b - 4 * a * c; //Высичтываем дискриминант
+
+            if (Math.Abs(discriminant) < 1e-10) //Тут возвращаем занчение которое в любом случае будет больше 0 (Math.Abs) и проверяем больше ли оно 0
+            {
+                double root = -b / (2 * a);
+                return new double[] { root };//Ну а тут если у нас один корень, то возвращаем новый массив с 1 корнем
+            }
+            else if (discriminant > 0)
+            {
+                double root1 = (-b + Math.Sqrt(discriminant)) / (2 * a);//тут то же самое что и там, Но тут 2 корня возведенные в квадрат (Math.Sqrt)
+                double root2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                return new double[] { root1, root2 };//Возврашем наши 2 корня записанные в массив
+            }
+            else
+            {
+                return new double[0];//Ну а если корень меньше 0, то возвращем 0, что приводит к завершению функции
+            }
+        }
+
+        public int GetDaysInYear(int year)
+        {
+            if (year <= 0)
+            {
+                throw new ArgumentException("Год должен буть больше 0!");//Если год меньше 0 то бросаем исключение
+            }
+
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) //Тут простые вычисления
+            {
+                return 366; //если правда, то возвращем что это - Весокосный год
+            }
+            else
+            {
+                return 365; //а если нет, то обычный год!
+            }
+        }
+
+        public bool ValidateEmail(string email) // Тут принимаем Email,в стрингах
+        {
+            string pattern = @"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$";//Ну тут я прям так не обьясню, тут нужно понимание
+            return Regex.IsMatch(email, pattern);//Тут сравниваем введенное значение с тес то должно получиться
+        }
+
+        public int CalculateDigitSum(string numberString)//Тут просто принимаем  числа в троке
+        {
+            int sum = 0;
+            foreach (char digitChar in numberString) //Перебираем их в чаре
+            {
+                if (char.IsDigit(digitChar)) //Ну а тут проверка на то что это число(Функция - IsDigit)
+                {
+                    int digit = int.Parse(digitChar.ToString()); //Приводим к Строке и пытаемся конвертировать в инт
+                    sum += digit; //Складываем все полученные числа
+                }
+            }
+            return sum; //Ну и возвращем по типу функции int - целое число
+        }
+    }
+}
---
+++ MathTaskClassLibrary/MathTaskClassLibraryTests/GeometryTests.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathTaskClassLibrary;
+using System;
+
+//Перед созданием обятаельно сверь чтобы Целевые Платформы  были одинковые тут Net Core 4.7
+
+namespace MathTaskClassLibraryTests
+{
+    [TestClass]
+    public class GeometryTests
+    {
+        [TestMethod] //Обязательно писать перед написаниями функции
+        public void CalculateAreaTest()
+        {
+            // Создаем переменные для последующего сравнения (Вместе с сразу прописанным результатом)
+            int a = 5;
+            int b = 3;
+            int expected = 15;
+
+            Geometry g = new Geometry(); //Тут мы просто  создаем обьект класса(в данном случае мы будем теперь обращться к Geometry как g)
+            int actual = g.CalculateArea(a, b); //Создаем переменную в которую записываем результат нашей функции
+
+            Assert.AreEqual(expected, actual); //Тут мы сравниваем (1 аргмент - сколько у насБ 2 по факту из функции)
+        }
+
+        [TestMethod]
+
+        public void CalculateAreaInvalidDataTest1() //Тут практически тоже самое, только мы ьберем блоки трай кетч то есть если у нас возникнет исключение то выбьет ошибку(Безопасно)
+        {
+            bool catched = false;
+            try
+            {
+                int a = -5;
+                int b = 3;
+
+                Geometry g = new Geometry();
+                g.CalculateArea(a, b);
+            }
+            catch(ArgumentException e)
+            {
+                catched = true;
+            }
+            Assert.IsTrue(catched, "Не обработанны исключения!");//Тут эта функция ожидает провала функции, и если тест по коду провалился, то тест выполнен
+        }
+
+        [TestMethod]
+        public void CalculateAreaInvalidDataTest2() //Ну а тут мы уже просто вызываем исключения из Assert, тут лучше прочто запомнить ка кэто делается
+        {
+            int a = -4;
+            int b = 10;
+
+            Geometry g = new Geometry();
+            Assert.ThrowsException<ArgumentException>(() => g.CalculateArea(a, b)); //Тут так же как и во 2
+        }
+    }
+
+    [TestClass]
+    public class AlphabetTest
+    {
+        [TestMethod]
+
+        public void AplhTesting() //Тут простое сравнение(a - 5 И expexted - 5 букв)
+        {
+            int a = 5;
+            string expected = "ABCDE";
+
+            Geometry g = new Geometry();
+            string actual = g.GenerateAlphabetString(a); //Переходим в функцию
+
+            Assert.AreEqual(expected, actual);//Сравниваем
+        }
+    }
+
+    [TestClass]
+    public class DiscTest
+    {
+        [TestMethod]
+
+        public void CalculateDisk() //Тут высчитываем дискриминант и если a < 0 то тогда фунция вызовет ArgumentException, что правильно!
+        {
+            bool catched = false;
+            try
+            {
+                double a = -1;
+                double b = 0;
+                double c = 4;
+
+                Geometry g = new Geometry();
+                g.SolveQuadraticEquation(a, b, c);
+            }
+            catch (ArgumentException e)
+            {
+                catched = true;
+            }
+            Assert.IsTrue(catched, "Не обработанны исключения!");
+        }
+    }
+
+
+    [TestClass]
+    public class YearTest
+    {
+        [TestMethod]
+
+        public void CalculateYear()//Там функция проверяет, весокосный ли год
+        {
+            int a = -1;
+
+            Geometry g = new Geometry();
+            Assert.ThrowsException<ArgumentException>(() => g.GetDaysInYear(a)); //Тут функция завершит свое исполнение если год < 0, как и в данном случае (он ждет ThrowsException<ArgumentException>), то есть если бы было любое другое, то был бы провал
+        }
+    }
+
+    [TestClass]
+    public class EmailTest
+    {
+        [TestMethod]
+
+        public void CheckEmail() //Тут так же банальное сравнение результата который думаешь ты - bool res = true; и как по факту - bool actual
+        {
+            bool res = true;
+            string expected = "test@test.test";
+
+            Geometry g = new Geometry();
+            bool actual = g.ValidateEmail(expected);
+            Assert.AreEqual(res, actual);
+        }
+    }
+
+    [TestClass]
+    public class StringTest //Тут тоже самое что и с прошлым, только тут высчитывают сумму
+    {
+        [TestMethod]
+
+        public void CalculateNum()
+        {
+            string str = "12345";
+            int result = 15;
+
+            Geometry g = new Geometry();
+            int res = g.CalculateDigitSum(str);
+            Assert.AreEqual(result, res);
+        }
+    }
+}
